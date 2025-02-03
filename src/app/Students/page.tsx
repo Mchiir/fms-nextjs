c361339d7e61f6706d5caca84c84f2704608bb57
Mchrispin <mchrispin14@gmail.com>
2025-02-04 01:24:25 +0200

    'resoved-slider-errors'

--- src/app/Students/page.tsx
+++ src/app/Students/page.tsx
@@ -1,13 +1,10 @@
 'use client'
 import React from "react";
 import ProgressList from "./components/ProgressList";
-import Slider from 'react-slick'
 import './components/styles.css'
 import Overall from "./components/Overall";
 import StudentHeader from "./components/StudentHeader";
 import StudentBody from "./components/StudentBody";
-
-
 
 function page(){
     return (
